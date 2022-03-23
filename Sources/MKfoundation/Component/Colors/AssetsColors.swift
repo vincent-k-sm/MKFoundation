0e92efcd198e35a2f10bc5c85af56c3a043aba8a
kim seungmin <kimtmdals@gmail.com>
2022-03-23 21:08:25 +0900

    Add ColorSets

---
+++ Sources/MKfoundation/Component/Colors/AssetsColors.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public enum Colors: String {
+
+    case background
+    case background_elevated
+    case background_tint
+    case black
+    case black_disabled
+    case black_elevated
+    case blue030
+    case blue050
+    case blue_primary
+    case divider
+    case green
+    case grey10
+    case grey100
+    case grey200
+    case grey300
+    case grey400
+    case grey50
+    case grey500
+    case grey600
+    case grey700
+    case grey800
+    case grey900
+    case orange
+    case pink
+    case purple100
+    case purple300
+    case purple50
+    case purple500
+    case purple700
+    case purple800
+    case purple900
+    case red
+    case text_disabled
+    case text_primary
+    case text_secondary
+    case textfield_bg
+    case white
+    case white_disabled
+    case white_elevated
+    case yellow
+}
