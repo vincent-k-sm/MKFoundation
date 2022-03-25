a3b815ef55e32374394248edbb63d643dc13dac0
kim seungmin <kimtmdals@gmail.com>
2022-03-26 08:09:57 +0900

    Add: Option Comments

--- Sources/MKfoundation/Component/MKCheckBox/Models/MKCheckBoxOptions.swift
+++ Sources/MKfoundation/Component/MKCheckBox/Models/MKCheckBoxOptions.swift
@@ -6,18 +6,24 @@
 import Foundation
 import UIKit
 
+/// 체크박스를 구성하는 값을 적용합니다
+/// - Parameters:
+///   - text : 컨텐츠 내용
+///   - isEnabled : enable 여부를 설정합니다
+///   - textColor : 제목 색상 (title Label에 직접 접근하여 변경할 수 있습니다)
+///   - isOn: On/Off Status
 public struct MKCheckBoxOptions {
     
-    public var text: String? = nil
-    public var isEnabled: Bool = true
+    public var text: String?     = nil
+    public var isEnabled: Bool   = true
     public var textColor: Colors = .text_primary
-    public var isOn: Bool = false
+    public var isOn: Bool        = false
     
     public init(
-        text: String? = nil,
-        isEnabled: Bool = true,
+        text: String?     = nil,
+        isEnabled: Bool   = true,
         textColor: Colors = .text_primary,
-        isOn: Bool = false
+        isOn: Bool        = false
     ) {
         self.text = text
         self.isEnabled = isEnabled
